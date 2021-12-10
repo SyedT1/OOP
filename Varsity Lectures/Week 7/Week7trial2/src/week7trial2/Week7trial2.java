b3e7fde19dc95089894fb58a30863b6c7d2f8c8f
SyedT1 <1830032@iub.edu.bd>
2021-12-10 17:42:04 +0600

    add

--- Varsity Lectures/Week 7/Week7trial2/src/week7trial2/Week7trial2.java
+++ Varsity Lectures/Week 7/Week7trial2/src/week7trial2/Week7trial2.java
@@ -5,6 +5,11 @@
  */
 package week7trial2;
 
+import java.io.PrintStream;
+import java.util.*;
+import java.lang.*;
+import usr.*;
+
 /**
  *
  * @author thinker
@@ -16,6 +21,17 @@ public class Week7trial2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Random c = new Random();
+        String[] names = {"Mark","Twain","Caliber","White","Q","Herbert"};
+        Person p = new Student().setall("IUB", "3.9", "IUB", "S", 10, 10);
+        p.viewall();
+        Person []q = new Person[10];
+        for(int i = 0;i<q.length;i++){
+            q[i] = new Student().setall("IUB", "3.9", "CS", names[c.nextInt(5)], i, i);
+        }
+        for(Person i:q){
+            i.viewall();
+        }
     }
-    
+
 }
