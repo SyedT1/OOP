3fbea1d9502963bf0f6599c51f5c77913f325cff
SyedT1 <1830032@iub.edu.bd>
2022-02-14 22:35:25 +0600

    modify

--- Varsity Lectures/Practice Problems 2/Test_Q1/src/test_q1/Test_Q1.java
+++ Varsity Lectures/Practice Problems 2/Test_Q1/src/test_q1/Test_Q1.java
@@ -1,4 +1,5 @@
 package test_q1;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -34,6 +35,12 @@ class Complex {
         this.img = img;
     }
 
+    @Override
+    public String toString() {
+        String res = new String();
+        return Integer.toString(this.real)+(this.img < 0 ? '-' : '+') + Integer.toString(this.img < 0 ? this.img * (-1) : this.img)+"i";
+    }
+
 }
 
 class ComplexList {
@@ -47,12 +54,30 @@ class ComplexList {
     public void add(Complex a) {
         cList.add(a);
     }
+
+    public void augment(Complex[] CArr) {
+        for (int i = 0; i < CArr.length; i++) {
+            CArr[i] = new Complex().setComplex();
+        }
+        for (Complex i : CArr) {
+            cList.add(i);
+        }
+    }
+
+    public void display(int low, int high) {
+        for (Complex i : cList) {
+            if (i.getReal() <= low && i.getImg() >= high) {
+                System.out.println(i);
+            }
+        }
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         String res = new String();
         System.out.println("Outputting the values inserted in the List:");
-        for(Complex i:cList){
-           res = res + i.getReal()+(i.getImg()>=0?'+':'-')+(i.getImg()<0?i.getImg()*-1:i.getImg())+'i'+'\n';
+        for (Complex i : cList) {
+            res = res + i.getReal() + (i.getImg() >= 0 ? '+' : '-') + (i.getImg() < 0 ? i.getImg() * -1 : i.getImg()) + 'i' + '\n';
         }
         return res;
     }
@@ -62,9 +87,9 @@ public class Test_Q1 {
 
     public static void main(String[] args) {
         ComplexList list = new ComplexList();
+        Scanner c = new Scanner(System.in);
         while (true) {
             System.out.println("choice 1: to add 2: to exit :");
-            Scanner c = new Scanner(System.in);
             int choice_val = c.nextInt();
             if (choice_val == 1) {
                 list.add(new Complex().setComplex());
@@ -74,6 +99,16 @@ public class Test_Q1 {
                 System.out.println("Wrong input. Type again");
             }
         }
-        System.out.println(list);
+        System.out.println("Ended");
+        System.out.println("Enter Additional Inputs = ");
+        int n = c.nextInt();
+        Complex[] cArr = new Complex[n];
+        list.augment(cArr);
+        int lower, upper;
+        System.out.println("Enter lower = ");
+        lower = c.nextInt();
+        System.out.println("Enter upper = ");
+        upper = c.nextInt();
+        list.display(lower, upper);
     }
 }
